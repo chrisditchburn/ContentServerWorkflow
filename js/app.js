446a8479dcfa9949f383d76ea83b27077e85cdb4
jasonaibrahim <jasonaibrahim@gmail.com>
2014-10-20 15:50:43 -0700

    added spinner and success message on mocked response

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@ app.directive('csFileUpload', function (otagService, $timeout) {
         link: function (scope, element, attrs) {
 
             var input = angular.element('<input type="file">'),
-                stateIndicator = element.find('img');
+                stateIndicator = element.find('img'),
+                stateIndicatorOriginalState = stateIndicator.attr('src');
 
             /**
              * define the actions that take place when this directive is clicked. this involves the following:
@@ -28,9 +29,19 @@ app.directive('csFileUpload', function (otagService, $timeout) {
 
             input.on('change', function (e) {
                 var file = e.target.files[0];
-                stateIndicator.attr('src', 'img/spinner.gif');
-                alert('file grabbed successfully.');
                 input.val('');
+                stateIndicator.attr('src', 'img/spinner.gif');
+
+                // TODO upload the file to content server and remove the spinner.
+                $timeout(function () {
+                    var textHelper = angular.element('<h3 class="text-success">Workflow started successfully</h3>');
+                    element.append(textHelper)
+                    $timeout(function () {
+                        stateIndicator.attr('src', stateIndicatorOriginalState);
+                        textHelper.remove();
+                    }, 1000);
+
+                }, 2000);
             });
 
             /** styling for the element provides feedback on user touch */
